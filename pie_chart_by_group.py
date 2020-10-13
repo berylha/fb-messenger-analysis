f319b1ccffbcee514f4bf37ce795ad047f5ebe10
Beryl Hovis-Afflerbach <berylha1332@gmail.com>
2020-10-13 12:29:54 -0500

    Consolidated extract_data.py and plots_group.py into pie_chart_by_group.py

--- pie_chart_by_group.py
+++ pie_chart_by_group.py
@@ -2,8 +2,23 @@ import numpy as np
 import pandas as pd
 import os
 import json
+import matplotlib.pyplot as plt
+from cycler import cycler
 
-print('Counting number of messages from each person in each chat')
+
+#### SPECIFY SETTINGS HERE: ####################################################
+
+# If you would like plots made only for specific chats, specify them here:
+# (Otherwise default will be all)
+chatlist = []
+
+# Setting style for plots; change fonts, colors if desired
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['axes.prop_cycle'] = cycler('color', ['#2364aa', '#3da5d6',
+            '#73bfb8', '#fec601', '#ea7317', '#f0f465', '#fab3a9', '#ed6b86',
+            '#a62639', '#db324d'])
+################################################################################
+
 
 # Setting directory to find message data
 # IMPORTANT: make sure you have extracted the folder into the same location as
@@ -14,16 +29,13 @@ dir = 'messages/inbox/'
 for chatfolder in os.listdir(dir):
     chatname = chatfolder.split('_')[0]
     all_files = os.listdir(dir + chatfolder)
-
     # Ensure that this chat has text-based messages
     files = [f for f in all_files if f.startswith('message')]
     if len(files) == 0:
         continue
-    print('    Extracting ' + chatname)
-
+    print('Extracting data from ' + chatname)
     # Define dictionary to store data
     data = {'participants': [], 'messages': []}
-
     # Iterate through JSON files; chats with >10000 messages have multiple
     for filename in files:
         data_new = json.load(open(dir + chatfolder + '/' + filename, 'r'))
@@ -31,7 +43,6 @@ for chatfolder in os.listdir(dir):
             data['participants'] = data['participants'] + data_new['participants']
         data['messages'] = data['messages'] + data_new['messages']
     senders = np.array([x['sender_name'] for x in data['messages']])
-
     # Move data to pandas dataframe
     df = pd.DataFrame(columns=['member','N'])
     # Count number of messages sent by each chat member
@@ -40,12 +51,15 @@ for chatfolder in os.listdir(dir):
     # Sort by number of messages
     df = df.sort_values(by=['N'], ignore_index=True)
 
-    # Determine which folder to save to depending on if 2-person or group chat
-    if len(data['participants']) <= 2:
-        to_save = 'count_single/' + chatname.replace('.json','') + '.dat'
-    else:
-        to_save = 'count_group/' + chatname.replace('.json','') + '.dat'
-    # Save file as CSV
-    df.to_csv(to_save, index=False, sep=' ')
-
-print('Finished extracting data')
+    # Now plot the pie chart
+    print('Plotting' + chatname)
+    tot = np.sum(df.N)
+    # Participant name only listed if sent >1% of messages
+    # (otherwise plot is too cluttered)
+    labels = [mem if df.N[i]>tot/100 else '' for i, mem in enumerate(df.member)]
+    # Generate pie chart of chat participants
+    plt.pie(df.N, labels=labels, rotatelabels=True)
+    plt.title(chatname, pad=70)
+    fig_name = 'plots_group/' + chatname + '.png'
+    plt.savefig(fig_name, bbox_inches='tight')
+    plt.close()
